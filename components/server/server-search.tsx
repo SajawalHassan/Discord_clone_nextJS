22e22d36ac51ffd85d962635be1b9abddad4eea1
sajawalhassan <sajawalhassan.1feb@gmail.com>
2023-10-26 09:14:11 +0500

    Add server search functionality

---
+++ components/server/server-search.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { Search } from "lucide-react";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+
+interface Props {
+  data: {
+    label: string;
+    type: "channel" | "member";
+    data:
+      | {
+          id: string;
+          name: string;
+          icon: React.ReactNode;
+        }[]
+      | undefined;
+  }[];
+}
+
+export const ServerSearch = ({ data }: Props) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const [open, setOpen] = useState<boolean>();
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+
+    return () => {
+      document.removeEventListener("keydown", down);
+    };
+  }, []);
+
+  const handleOnClick = ({
+    id,
+    type,
+  }: {
+    id: string;
+    type: "channel" | "member";
+  }) => {
+    setOpen(false);
+
+    if (type === "member")
+      router.push(`/servers/${params?.serverId}/conversations/${id}`);
+    if (type === "channel")
+      router.push(`/servers/${params?.serverId}/channels/${id}`);
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+      >
+        <Search className="h-4 w-4 text-zinc-500 dark:text-zinc-400" />
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+          Search
+        </p>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">CTRL</span>K
+        </kbd>
+      </button>
+      <CommandDialog onOpenChange={setOpen} open={open}>
+        <CommandInput placeholder="Search channels or members" />
+        <CommandList>
+          <CommandEmpty>No results found</CommandEmpty>
+          {data?.map(({ type, label, data }) => {
+            if (!data?.length) return null;
+
+            return (
+              <CommandGroup key={label} heading={label}>
+                {data?.map(({ id, icon, name }) => (
+                  <CommandItem
+                    key={id}
+                    onSelect={() => handleOnClick({ id, type })}
+                  >
+                    {icon}
+                    {name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            );
+          })}
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
