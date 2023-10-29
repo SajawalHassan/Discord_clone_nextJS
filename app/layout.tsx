709b7cf6dd86356c627c843db4a5ca7d0cf35c13
sajawalhassan <sajawalhassan.1feb@gmail.com>
2023-10-29 19:08:19 +0500

    Add socketio

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,29 +3,23 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalsProvider } from "@/components/providers/modals-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 export const metadata: Metadata = {
   title: "Discord Clone",
   description: "A discord clone",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className="bg-white dark:bg-[#313338]">
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem={true}
-            storageKey="discord-clone-theme"
-          >
-            <ModalsProvider />
-            {children}
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true} storageKey="discord-clone-theme">
+            <SocketProvider>
+              <ModalsProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
+import { SocketIndicator } from "@/components/socket-indicator";
 
 interface Props {
   serverId: string;
@@ -16,6 +17,9 @@ export const ChatHeader = ({ name, serverId, type, imageUrl }: Props) => {
       {type === "channel" && <Hash className="w-5 h-5 to-zinc-500 dark:text-zinc-400 mr-1" />}
       {type == "conversation" && <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />}
       <p className="font-semibold text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
